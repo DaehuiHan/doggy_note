804e1ff2a45f8fa06cfef2b743f523e0be5566da
DaehuiHan <79817611+DaehuiHan@users.noreply.github.com>
2021-03-15 23:00:23 +0900

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+DATABASE_CONFIG = {
+    'host': '15.164.103.150',
+    'dbname': 'doggynotedb',
+    'port': 27017,
+    'sceret_key': 'DOGGYNOTE',
+    'user':'test',
+    "password":'test'
+    }
